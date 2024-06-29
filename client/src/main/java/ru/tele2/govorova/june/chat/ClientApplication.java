9718b6dd1b38555b5d585385b3103f9d53d078b1
KristinaGovorova <k.govorova94@mail.ru>
2024-06-29 21:15:57 +0300

    init commit

---
+++ client/src/main/java/ru/tele2/govorova/june/chat/ClientApplication.java
@@ -0,0 +1,9 @@
+package ru.tele2.govorova.june.chat;
+
+import java.io.IOException;
+
+public class ClientApplication {
+    public static void main(String[] args) throws IOException {
+        new Client();
+    }
+}
